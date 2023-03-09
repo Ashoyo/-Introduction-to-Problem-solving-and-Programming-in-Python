29d454e3b793e890a2604a602ef11f6360875279
Shoyo Jiang <81840394+Ashoyo@users.noreply.github.com>
2023-03-09 17:37:17 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Mark Zukerberg is one of the most famous computer scientist and the ceo of 
+facbook!")
+print("He developed an interest in computer programming at an early age,")
+print("Lastly,Mark Zukerberg invented facebook in his sophomore year of college 
+with his colleges,which changed the lives of many.")
+print("    ((`\  ")
+print(" ___ \\\ '--._")
+print(".'`   `'    o  )")      #its a bunny i tried
+print("/    \   '. __.'")
+print("_|    /_  \ \_\_")
+people = input("have you ever heard of Mark zukerberg:")
+Mark =input("they are  pretty cool arent they")
+print(Mark)
