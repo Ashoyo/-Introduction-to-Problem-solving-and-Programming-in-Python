6d558f189cc8a48f50b021cdf91792591421419f
Shoyo Jiang <81840394+Ashoyo@users.noreply.github.com>
2023-03-09 17:43:03 -0500

    Create piglatin.py

---
+++ piglatin.py
@@ -0,0 +1,17 @@
+vowels = ["a","e","i","o","u"]
+def findFirstVowel(word):
+    for c in range(len(word)):
+        if word(c) in vowels:
+            return c
+        return -1
+def main():
+    while True:
+     words = input("Please type what you would like converted to Pig Latin: 
+").lower()
+     if word == "done":
+      break
+def convertToPigLatin():
+    x = findFirstVowel(word)
+    if x == -1:
+        print(word)
+        print(reverse(word))
